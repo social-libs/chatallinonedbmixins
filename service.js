9a6aaf55f86bb036351c18e29856be76023c12e5
Andrija Petrovic <andrija.hers@gmail.com>
2020-03-16 14:20:32 +0000

    servicesToStartStatically is now a function

--- service.js
+++ service.js
@@ -8,13 +8,26 @@ function createServiceMixin (execlib) {
   ChatAllInOneDBServiceMixin.prototype.destroy = lib.dummyFunc;
 
   //static
-  ChatAllInOneDBServiceMixin.servicesToStartStatically = [{
-    //modulename: 'social__data_chatservice',
-    modulename: 'social_chatservice',
-    instancename: 'Chat',
-    propertyhash: {
+  function chatadder (result, chatname) {
+    if (lib.isString(chatname) && chatname.length>0) {
+      result.push({
+        modulename: 'social_chatservice',
+        instancename: chatname,
+        propertyhash: {
+          chatdbpath: chatname+'.db'
+        }
+      });
     }
-  }];
+    return result;
+  }
+
+  ChatAllInOneDBServiceMixin.servicesToStartStatically = function (options) {
+    var ret = [];
+    if (options && lib.isArray(options.chats)) {
+      options.chats.reduce(chatadder, ret);
+    }
+    return ret;
+  };
 
   ChatAllInOneDBServiceMixin.addMethods = lib.dummyFunc;
 
